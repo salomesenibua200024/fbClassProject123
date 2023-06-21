88cfc6015bbd02f09d7bc1bc6f7ae5bea331504a
salomesenibua200024 <salomesenibua@yahoo.com>
2023-06-22 03:24:25 +1000

    added home and about with router

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import { initializeApp } from 'firebase/app'
 
 import './App.css';
 import { Test } from './components/Test'; 
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Routes, Route } from 'react-router-dom';
 
 const firebaseapp = initializeApp(firebaseConfig)
 
 function App() {
+
   return (
     <div className="App">
-      <Test />
-      <Test />
-      <div />
-      );
-}
-
-      export default TypesExample;
+      <Routes>
+        <Route path="/" element={ <Home/> }  />  
+        <Route path="/about" element={ <About/> } />
+      </Routes>
     </div>
   );
 }
