e0130d948e74f923f83d8c7a2bac0e0a1d299147
salomesenibua200024 <salomesenibua@yahoo.com>
2023-06-22 01:35:13 +1000

    added another Test element

--- src/App.js
+++ src/App.js
@@ -2,22 +2,15 @@ import { firebaseConfig } from './config/Config';
 import { initializeApp } from 'firebase/app'
 
 import './App.css';
+import { Test } from './components/Test'; 
 
 const firebaseapp = initializeApp(firebaseConfig)
 
 function App() {
   return (
     <div className="App">
-      <button type="button" class="btn btn-primary">Primary</button>
-      <button type="button" class="btn btn-secondary">Secondary</button>
-      <button type="button" class="btn btn-success">Success</button>
-      <button type="button" class="btn btn-danger">Danger</button>
-      <button type="button" class="btn btn-warning">Warning</button>
-      <button type="button" class="btn btn-info">Info</button>
-      <button type="button" class="btn btn-light">Light</button>
-      <button type="button" class="btn btn-dark">Dark</button>
-
-      <button type="button" class="btn btn-link">Link</button>
+      <Test />
+      <Test />
       <div />
       );
 }
